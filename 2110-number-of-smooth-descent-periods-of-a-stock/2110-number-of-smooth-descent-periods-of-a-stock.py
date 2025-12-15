28b3d0425da9bb2f81aa973949690ada480cb22b
Shashank0701-byte <shashankchakraborty712005@gmail.com>
2025-12-15 12:34:46 +0530

    Time: 62 ms (48.51%), Space: 29.6 MB (90.3%) - LeetHub

---
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getDescentPeriods(self, prices: List[int]) -> int:
+        ans = 1
+        streak = 1
+        
+        for i in range(1, len(prices)):
+            if prices[i] == prices[i-1] - 1:
+                streak += 1
+            else:
+                streak = 1
+            ans += streak
+        
+        return ans
